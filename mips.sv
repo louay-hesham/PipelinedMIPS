2b05c0bb966bfef040a3407bc98aa41c228683a9
louay-hesham <louay.hesham@gmail.com>
2017-05-10 19:41:46 +0200

    MIPS module pipelined

--- mips.sv
+++ mips.sv
@@ -1,14 +1,25 @@
 module mips(	input logic clk, reset,
-		output logic [31:0] pc,
-		input logic [31:0]instr, 
-		output logic memwrite, 
-		output logic [31:0] aluout, writedata, 
-		input logic [31:0] readdata);
-	logic memtoreg, alusrc, regdst, regwrite, jump, signOrZero, pcsrc, zero; 
-	logic [2:0] alucontrol;
-	controller c(	instr[31:26], instr[5:0], zero, memtoreg,
-			memwrite, pcsrc, alusrc, regdst, regwrite, jump, signOrZero, alucontrol);
-	datapath dp(	clk, reset, memtoreg, pcsrc, alusrc, regdst,
-			regwrite, jump, signOrZero, alucontrol, zero, pc, instr, aluout, writedata, readdata);
-endmodule
+ 		output logic [31:0] pcF,
+ 		input logic [31:0] instrF,
+		output logic memwriteM,
+		output logic [31:0] aluoutM, writedataM,
+		input logic [31:0] readdataM);
+	logic [5:0] opD, functD;
+ 	logic regdstE, alusrcE, pcsrcD, memtoregE, memtoregM, memtoregW, regwriteE, regwriteM, regwriteW;
+ 	logic [2:0] alucontrolE;
+ 	logic flushE, equalD;
 
+	controller c(	clk, reset, opD, functD, flushE,
+			equalD,memtoregE, memtoregM,
+ 			memtoregW, memwriteM, pcsrcD,
+ 			branchD,alusrcE, regdstE, regwriteE,
+ 			regwriteM, regwriteW, jumpD, alucontrolE);
+
+ 	datapath dp(	clk, reset, memtoregE, memtoregM,
+ 			memtoregW, pcsrcD, branchD,
+ 			alusrcE, regdstE, regwriteE,
+ 			regwriteM, regwriteW, jumpD,
+ 			alucontrolE, equalD, pcF, instrF,
+ 			aluoutM, writedataM, readdataM,
+ 			opD, functD, flushE);
+endmodule
