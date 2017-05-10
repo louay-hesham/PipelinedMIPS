ceb03a9583256ca246ce537f4ecc1fe59ad89cd3
louay-hesham <louay.hesham@gmail.com>
2017-05-10 19:34:38 +0200

    added single cycle modules

---
+++ controller.sv
@@ -0,0 +1,14 @@
+module controller(	input logic [5:0] op, funct,
+			input logic zero,
+			output logic memtoreg, memwrite,
+			output logic pcsrc, alusrc,
+			output logic regdst, regwrite, 
+			output logic jump, signOrZero,
+			output logic [2:0] alucontrol);
+
+	logic [1:0] aluop;
+	logic branch, bne;
+	maindec md(op, memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, bne, signOrZero, aluop);
+	aludec ad(funct, aluop, alucontrol);
+	assign pcsrc = (branch & zero) | (bne & ~zero);
+endmodule
