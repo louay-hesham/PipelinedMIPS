1f5073d938a1c948c85af973366c0048df8e7101
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2017-05-12 22:50:30 +0200

    Alu edited

--- aludecoder.sv
+++ aludecoder.sv
@@ -2,17 +2,17 @@ module aludec(	input logic [5:0] funct,
 		input logic [1:0] aluop,
 		output logic [2:0] alucontrol);
 	always_comb
-		case(aluop)
-			2'b00: alucontrol <= 3'b010; 	// add (for lw/sw/addi)
-			2'b01: alucontrol <= 3'b110; 	// sub (for beq)
-			default:
-				case(funct) 		// R-type instructions
-					6'b100000: alucontrol <= 3'b010; // add
-					6'b100010: alucontrol <= 3'b110; // sub
-					6'b100100: alucontrol <= 3'b000; // and
-					6'b100101: alucontrol <= 3'b001; // or
-					6'b101010: alucontrol <= 3'b111; // slt
-					default: alucontrol <= 3'bxxx;   // ???
-				endcase
+		case (aluop)
+			2'b00: alucontrol <= 3'b010; // add (lw,sw,addi)
+			2'b01: alucontrol <= 3'b110; // sub (beq)
+			2'b11: alucontrol <= 3'b001; //ori
+			default: case(funct) // RTYPE
+			6'b100000: alucontrol <= 3'b010; // ADD
+			6'b100010: alucontrol <= 3'b110; // SUB
+			6'b100100: alucontrol <= 3'b000; // AND
+			6'b100101: alucontrol <= 3'b001; // OR
+			6'b101010: alucontrol <= 3'b111; // SLT
+			default: alucontrol <= 3'bxxx; // ???
+			endcase
 		endcase
 endmodule
--- maindecoder.sv
+++ maindecoder.sv
@@ -5,18 +5,21 @@ module maindec(	input logic [5:0] op,
  		output logic jump,
  		output logic [1:0] aluop);
 
- 	logic [8:0] controls;
- 	assign {regwrite, regdst, alusrc, branch, memwrite, memtoreg, jump, aluop} = controls;
- 	
-	always_comb
- 		case(op)
- 			6'b000000: controls <= 9'b110000010; //Rtyp
- 			6'b100011: controls <= 9'b101001000; //LW
- 			6'b101011: controls <= 9'b001010000; //SW
- 			6'b000100: controls <= 9'b000100001; //BEQ
- 			6'b001000: controls <= 9'b101000000; //ADDI
- 			6'b000010: controls <= 9'b000000100; //J
- 			default: controls <= 9'bxxxxxxxxx; //???
- 		endcase
+ 	reg [10:0] controls;
+		assign {bne,ori,regwrite, regdst, alusrc,
+					branch, memwrite,
+					memtoreg, jump, aluop} =controls;
+	always @ (*)
+	case(op) // cases of opcode
+		6'b000000: controls <=11'b00110000010; //R-type
+		6'b100011: controls <=11'b00101001000; //LW
+		6'b101011: controls <=11'b00001010000; //SW
+		6'b000100: controls <=11'b00000100001; //BEQ
+		6'b001000: controls <=11'b00101000000; //ADDI
+		6'b000010: controls <=11'b00000000100; //J
+		6'b001101: controls <=11'b01101000011; //ori alu opcode = 11, signal ori is on
+		6'b000101: controls <=11'b10000000001; // BNE same alu opcode as branch 
+		default:   controls <=11'bxxxxxxxxxxx; //illegal op
+		endcase
 
 endmodule
