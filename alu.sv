e4893c8224f4257667d76cc30521e67833a6cd23
louay-hesham <louay.hesham@gmail.com>
2017-05-22 15:39:06 +0200

    used <= instead of = for correct assignment

--- alu.sv
+++ alu.sv
@@ -6,17 +6,16 @@ module alu(input  logic [31:0] a, b,
 
 	always_comb // combinational msh sequential 
 		case(f)
-			0: y = a & b;
-			1: y = a | b;
-			2: y = a + b;
-			3: y = 7'b0000_0000; 
-			4: y = a & ~ b; 
-			5: y = a | ~ b;
-			6: y = a - b;
+			0: y <= a & b;
+			1: y <= a | b;
+			2: y <= a + b;
+			3: y <= 7'bz; 
+			4: y <= a & ~ b; 
+			5: y <= a | ~ b;
+			6: y <= a - b;
 			7: if($signed(a) < $signed(b)) 
-				y = 1;
+				y <= 1;
 			   else
-				y = 0;
-			default: y = 32'bx;
+				y <= 0;
 		endcase
 endmodule
