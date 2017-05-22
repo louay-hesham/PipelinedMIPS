c8815ea80f7c428879141f7b5fb51c86e041b726
louay-hesham <louay.hesham@gmail.com>
2017-05-22 15:16:01 +0200

    modified alu decoder to have mult and div operations

--- aludecoder.sv
+++ aludecoder.sv
@@ -7,12 +7,15 @@ module aludec(	input logic [5:0] funct,
 			2'b01: alucontrol <= 3'b110; // sub (beq)
 			2'b11: alucontrol <= 3'b001; //ori
 			default: case(funct) // RTYPE
-			6'b100000: alucontrol <= 3'b010; // ADD
-			6'b100010: alucontrol <= 3'b110; // SUB
-			6'b100100: alucontrol <= 3'b000; // AND
-			6'b100101: alucontrol <= 3'b001; // OR
-			6'b101010: alucontrol <= 3'b111; // SLT
-			default: alucontrol <= 3'bxxx; // ???
+				6'b100100: alucontrol <= 3'b000; // AND
+				6'b100101: alucontrol <= 3'b001; // OR
+				6'b100000: alucontrol <= 3'b010; // ADD
+				6'b011000: alucontrol <= 3'b011; // MULT
+				6'b011010: alucontrol <= 3'b100; // DIV
+
+				6'b100010: alucontrol <= 3'b110; // SUB
+				6'b101010: alucontrol <= 3'b111; // SLT
+				default:   alucontrol <= 3'bxxx; // ???
 			endcase
 		endcase
 endmodule
