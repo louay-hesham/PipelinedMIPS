2825aef29007f8808af4edcb005b6699e4cb331d
MahinourElsheikh <mahinour-elsheikh@hotmail.com>
2017-06-02 13:47:36 +0200

    control unit mods for shift

--- controller.sv
+++ controller.sv
@@ -6,25 +6,25 @@ module controller(	input logic clk, reset,
  			output logic pcsrcD, branchD, bneD, alusrcE,
  			output logic [1:0]regdstE, 
  			output logic regwriteE, regwriteM, regwriteW,
- 			output logic jumpD, jrD,
+ 			output logic jumpD, jrD,shiftE,
  			output logic [3:0] alucontrolE,
 			output logic hienE, loenE);
  	
 	logic [1:0] aluopD, regdstD, memtoregD;
- 	logic memwriteD, alusrcD, regwriteD, hienD, lowenD;
+ 	logic memwriteD, alusrcD, regwriteD, hienD, lowenD,shiftD;
  	logic [3:0] alucontrolD;
  	logic memwriteE;
       // signales in stage D are out
 	maindec md(opD, memtoregD, memwriteD, branchD, bneD,
  			alusrcD, regdstD, regwriteD, jumpD, aluopD);
  
-	aludec ad(functD, aluopD, alucontrolD, hienD, loenD, jrD);
+	aludec ad(functD, aluopD, alucontrolD, hienD, loenD, jrD,shiftD);
         //early branch resolution
 	assign pcsrcD = (branchD & equalD) | (bneD & ~equalD);
  	//decode to execute controls transition
-        floprc #(13)   regExecute ( clk,reset,flushE,
-					{regwriteD,memtoregD,memwriteD,alucontrolD,alusrcD,regdstD, hienD, loenD},
-					{regwriteE,memtoregE,memwriteE,alucontrolE,alusrcE,regdstE, hienE, loenE});
+        floprc #(14)   regExecute ( clk,reset,flushE,
+					{regwriteD,memtoregD,memwriteD,alucontrolD,alusrcD,regdstD, hienD, loenD,shiftD},
+					{regwriteE,memtoregE,memwriteE,alucontrolE,alusrcE,regdstE, hienE, loenE,shiftE});
 					
  	//execute to memw  controls transition
 	flopr #(4) regMem(clk, reset,
